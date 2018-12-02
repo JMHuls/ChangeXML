c5584968d673de8bfecab1e6325259f03cbf1ed2
JHuls <JHuls@TB-CHB-03.coenhagedoornbouw.nl>
2018-12-02 17:06:49 +0100

    Final version
    With error handling for:
    - corrupt XML
    - wrong XML
    - XML that won't need correction

--- TradXML/TradXML.py
+++ TradXML/TradXML.py
@@ -18,6 +18,10 @@ def line_prepender(filename, line):
 def Mbox(title, text, style):
     return ctypes.windll.user32.MessageBoxW(0, text, title, style)
 
+#  Specify tags
+RightTag = 'Tradbegroting4'
+WrongTag = 'TradbegrotingIbis'
+
 # Create a PyQt4 application and widget
 a = QApplication(sys.argv)
 w = QWidget()
@@ -29,35 +33,43 @@ w.setWindowTitle("Open XML")
 # Get filename
 File = QFileDialog.getOpenFileName(w, 'Open XML', '/')
 if len(File) > 0:
+    # If file is a XML
     if str(File).lower().endswith('.xml'):
+        # Check if original file isn't corrupt
+        try:
+            tree = ET.parse(File)
+        except:
+            Mbox(u'XML niet aangepast', u'Corrupte XML ingevoerd', 0)
+            sys.exit()
+            
         # Open original file
         tree = ET.parse(File)
         root = tree.getroot()
         i = 0
         c = 0
         
-        # Edit file
-        for ibis in root.iter('TradbegrotingIbis'):
-            ibis.tag = str('Tradbegroting4')
-            i = i + 1
-            
-        for check in root.iter('Tradbegroting4'):
+        # Check tags
+        for check in root.iter(RightTag):
             c = c + 1
         
-        # Write back to file
-        #tree.write(File, encoding="utf-8", xml_declaration=True)
-        tree.write(File)
-        
-        line_prepender(File, '<?xml version="1.0" encoding="utf-8" standalone="yes"?>')
-        
-        if i >= 1:
-            if i == 1:
-                Mbox(u'XML aangepast', u'Conversie gelukt, de XML kan ingelezen worden in BouwVision', 0)
-            else:
-                Mbox(u'XML niet aangepast', u'Conversie mislukt', 0)
+        # If RightTag is found
+        if c >= 1:
+            Mbox(u'XML niet aangepast', u'De XML lijkt al correct te zijn en zou in BouwVision ingelezen moeten kunnen worden', 0)
+        # If RightTag is not found
         else:
-            if c >= 1:
-                Mbox(u'XML niet aangepast', u'DeXML lijkt al correct te zijn en zou in BouwVision ingelezen moeten kunnen worden', 0)
+            # Count and change tags, do not write to file yet
+            for ibis in root.iter(WrongTag):
+                ibis.tag = str(RightTag)
+                i = i + 1
+            
+            if i >= 1:
+                if i == 1:
+                    # Write back to file if file and tags are confirmed
+                    tree.write(File)
+                    line_prepender(File, '<?xml version="1.0" encoding="utf-8" standalone="yes"?>')
+                    Mbox(u'XML aangepast', u'Conversie gelukt, de XML kan ingelezen worden in BouwVision', 0)
+                else:
+                    Mbox(u'XML niet aangepast', u'Conversie mislukt', 0)
             else:
                 Mbox(u'XML niet aangepast', u'Ongeldige XML ingevoerd', 0)
     else:
@@ -65,5 +77,5 @@ if len(File) > 0:
 else:
     Mbox(u'Open XML', u'Geen bestand gekozen', 0)
     
-#sys.exit(a.exec_())
+# Stop this script
 sys.exit()
